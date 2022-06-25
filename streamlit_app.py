fdfd7f13f06901e85d138bdf53dd2c463e61421e
Zulaikhaj <51526768+Zulaikhaj@users.noreply.github.com>
2022-06-26 00:10:13 +0500

    Update streamlit_app.py
    added title
    added breakfast details

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Dinner")
+
+streamlit.subheader("Breakfast Menu")
+streamlit.write("Omega 3 & Blueberry Oatmeal")
+streamlit.write("Kale Spinach & Rocket Smoothie")
+streamlit.write("Hard-boiled Free-Range Egg")
